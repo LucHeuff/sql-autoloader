efca8c51ad379a9b22104f6a55f72e40cac5b77c
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-06-22 17:49:36 +0200

    fix: added docstrings and ignored missing type annotation (that is deliberate)

--- etl_components/convertors.py
+++ etl_components/convertors.py
@@ -3,9 +3,31 @@ from typing import Any, Callable
 DataframeToDict = Callable[[Any], list[dict]]
 
 
-def pandas_to_dict(df) -> list[dict]:
+def pandas_to_dict(df) -> list[dict]:  # noqa: ANN001
+    """Convert a pandas dataframe to a list of dicts.
+
+    Args:
+    ----
+        df: pandas dataframe
+
+    Returns:
+    -------
+        list of dicts
+
+    """
     return df.to_dict(orient="records")
 
 
-def polars_to_dict(df) -> list[dict]:
+def polars_to_dict(df) -> list[dict]:  # noqa: ANN001
+    """Convert a polars dataframe to a list of dicts.
+
+    Args:
+    ----
+        df: polars dataframe
+
+    Returns:
+    -------
+        list of dicts
+
+    """
     return df.to_dicts()
