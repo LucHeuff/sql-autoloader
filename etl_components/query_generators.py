3cd3d057e9defc7542c09ac2a665bea58d582e72
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-07-13 21:01:47 +0200

    feature: added insert query generator

--- etl_components/query_generators.py
+++ etl_components/query_generators.py
@@ -1,3 +1,37 @@
+from typing import Callable
+
+
+def create_insert_query(
+    table: str,
+    columns: list[str],
+    insert_prefix: str,
+    insert_postfix: str,
+    parameterize_method: Callable[[str], str],
+) -> str:
+    """Create an insert query for this database connector.
+
+    To be used in combination with for example sqlite3, psycopg, etc.
+
+    Args:
+    ----
+        table: to be inserted into
+        columns: to be inserted
+        insert_prefix: allows passing a prefix before INTO
+        insert_postfix: allows passing a postfix after VALUES
+        parameterize_method: used to convert the column names into
+                             the parameterized format for this database connector
+
+    Returns:
+    -------
+        valid insert query for this connector
+
+
+    """
+    columns_section = ", ".join(columns)
+    values_section = ", ".join([parameterize_method(col) for col in columns])
+    return f"{insert_prefix} INTO {table} ({columns_section})\nVALUES ({values_section}){insert_postfix}".strip()
+
+
 def create_retrieve_query(table: str, columns: list[str]) -> str:
     """Create a retrieve query for this table and columns.
 
--- tests/test_query_generators.py
+++ tests/test_query_generators.py
@@ -1,4 +1,47 @@
-from etl_components.query_generators import create_retrieve_query
+from etl_components.query_generators import (
+    create_insert_query,
+    create_retrieve_query,
+)
+
+
+def test_create_insert_query_sqlite() -> None:
+    """Test whether create_insert_query works as intended for sqlite3 connectors."""
+    table = "test"
+    columns = ["test1", "test2", "test3", "test4"]
+    insert_prefix = "INSERT OR IGNORE"
+    insert_postfix = ""
+
+    def param_method(column: str) -> str:
+        return f":{column}"
+
+    query = "INSERT OR IGNORE INTO test (test1, test2, test3, test4)\nVALUES (:test1, :test2, :test3, :test4)"
+
+    assert (
+        create_insert_query(
+            table, columns, insert_prefix, insert_postfix, param_method
+        )
+        == query.strip()
+    )
+
+
+def test_create_insert_query_postgres() -> None:
+    """Test whether create_insert_quer works as intended for psycopg connectors."""
+    table = "test"
+    columns = ["test1", "test2", "test3", "test4"]
+    insert_prefix = "INSERT"
+    insert_postfix = "\nON CONFLICT DO NOTHING"
+
+    def param_method(column: str) -> str:
+        return f"%({column})s"
+
+    query = "INSERT INTO test (test1, test2, test3, test4)\nVALUES (%(test1)s, %(test2)s, %(test3)s, %(test4)s)\nON CONFLICT DO NOTHING"
+
+    assert (
+        create_insert_query(
+            table, columns, insert_prefix, insert_postfix, param_method
+        )
+        == query.strip()
+    )
 
 
 def test_create_retrieve_query() -> None:
