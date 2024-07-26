18509321e75c9349c20f7d3f9bdd9fef59b2214b
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-07-26 21:51:59 +0200

    fix: removed imports for old modules

--- etl_components/__init__.py
+++ etl_components/__init__.py
@@ -1,3 +1,3 @@
-from etl_components.connections import PostgresCursor, SQLiteCursor
-from etl_components.interactions import insert, retrieve_ids, insert_and_retrieve_ids, compare
+# from etl_components.connections import PostgresCursor, SQLiteCursor
+# from etl_components.interactions import insert, retrieve_ids, insert_and_retrieve_ids, compare
 
