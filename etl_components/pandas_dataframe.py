f57566addf32855c0aa5086235acb58655215376
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-07-27 14:21:01 +0200

    feature: added support for pandas dataframes

---
+++ etl_components/pandas_dataframe.py
@@ -0,0 +1,108 @@
+import pandas as pd
+
+
+class MissingIDsError(Exception):
+    """Raised when merging data from the db results in missing values in id columns."""
+
+
+class PandasDataFrame:
+    """Wrapper for pandas dataframes."""
+
+    def __init__(self, data: pd.DataFrame) -> None:
+        """PandasDataframe constructor."""
+        self.df = data
+
+    def rename(self, mapping: dict[str, str]) -> None:
+        """Rename columns.
+
+        Args:
+        ----
+            mapping: dictionary of {old_name: new_name}
+
+        """
+        self.df = self.df.rename(columns=mapping)
+
+    def drop(self, columns: list[str]) -> None:
+        """Drop columns from data.
+
+        Args:
+        ----
+            columns: list of columns to be dropped
+
+        """
+        self.df = self.df.drop(columns=columns, errors="ignore")
+
+    def rows(self, columns: list[str]) -> list[dict]:
+        """Return unique rows from the data, for the selected columns.
+
+        Args:
+        ----
+            columns: list of columns to restrict the data to.
+
+        """
+        return self.df.filter(items=columns).to_dict("records")
+
+    def merge_ids(
+        self,
+        db_fetch: list[dict],
+        *,
+        allow_duplication: bool = False,
+    ) -> pd.DataFrame:
+        """Merge data with ids from database with data, using polars.
+
+        Args:
+        ----
+            data: DataFrame where ids are to be merged into
+            db_fetch: output from cursor.fetchall()
+            allow_duplication: (Optional) whether merging ids is allowed to duplicate rows.
+                                Default: false
+
+        Returns:
+        -------
+            pl.DataFrame merged with ids from database
+
+
+        Raises:
+        ------
+            MissingIDsError: if as a result of merging any *_id columns now contain misings.
+
+        """
+        orig_len = len(self.df)
+        # determining data types in self.df to make sure I don't get errors with dtypes
+        schema = self.df.dtypes.to_dict()
+        # attempting to replace None since pandas allows multiple values for that
+        db_data = (
+            pd.DataFrame(db_fetch)
+            .replace({float("nan"): None, "": None})
+            .astype(schema)
+        )
+
+        # taking the columns the two datasets have in common as join columns
+        on_columns = list(set(self.columns) & set(db_data.columns))
+
+        data = self.df.merge(db_data, on=on_columns, how="left")
+
+        # sanity check: dataset should not shrink (impossible?) and rows should not be duplicated
+        assert not len(data) < orig_len, "Rows were lost when joining on ids."
+        assert (
+            not len(data) > orig_len or allow_duplication
+        ), "Rows were duplicated when joining on ids."
+
+        # checking if any of the id columns are now empty
+        missing_ids = data.filter(regex="_id$").isna()
+        if bool(missing_ids.any(axis=None)):
+            rows_with_missings = data[missing_ids.any(axis=1)]
+            message = "Some id's were returned as NA:\n"
+            raise MissingIDsError(message + str(rows_with_missings))
+
+        return data
+
+    @property
+    def columns(self) -> list[str]:
+        """Returns a list of column names."""
+        return self.df.columns.tolist()
+
+    @property
+    def data(self) -> pd.DataFrame:
+        """Returns the data as the original dataframe type."""
+        return self.df
---
+++ tests/test_pandas_dataframe.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
+
+from etl_components.pandas_dataframe import PandasDataFrame
+
+
+def test_columns() -> None:
+    """Test PandasDataFrame.columns returns the correct columns."""
+    pandas_df = PandasDataFrame(pd.DataFrame({"a": [1], "b": [1], "c": [1]}))
+    assert pandas_df.columns == ["a", "b", "c"]
+
+
+def test_data() -> None:
+    """Test PandasDataFrame.data returns the original pandas dataframe."""
+    pd_df = pd.DataFrame({"a": [1], "b": [1], "c": [1]})
+    pandas_df = PandasDataFrame(pd_df)
+    assert_frame_equal(pd_df, pandas_df.data)
+
+
+def test_rename() -> None:
+    """Test PandasDataFrame.rename() correctly renames the data."""
+    pandas_df = PandasDataFrame(pd.DataFrame({"a": [1], "b": [1], "c": [1]}))
+    pandas_df.rename({"a": "A", "b": "B", "c": "C"})
+    assert pandas_df.columns == ["A", "B", "C"]
+
+
+def test_drop() -> None:
+    """Test PandasDataFrame.drop() correctly drops columns."""
+    pandas_df = PandasDataFrame(pd.DataFrame({"a": [1], "b": [1], "c": [1]}))
+    pandas_df.drop(["b", "c"])
+    assert pandas_df.columns == ["a"]
+
+
+def test_rows() -> None:
+    """Test PandasDataFrame.rows() correctly returns rows."""
+    pandas_df = PandasDataFrame(pd.DataFrame({"a": [1], "b": [1], "c": [1]}))
+    assert pandas_df.rows(["a", "b", "c"]) == [{"a": 1, "b": 1, "c": 1}]
+    # testing if it also works when subselecting some data
+    assert pandas_df.rows(["a", "c"]) == [{"a": 1, "c": 1}]
+
+
+def test_merge_ids() -> None:
+    """Test PandasDataFrame.merge_ids() correctly merges ids."""
+    pandas_df = PandasDataFrame(pd.DataFrame({"a": ["A", "B", "C"]}))
+    db_fetch = [
+        {"a_id": 1, "a": "A"},
+        {"a_id": 2, "a": "B"},
+        {"a_id": 3, "a": "C"},
+    ]
+    merge = pandas_df.merge_ids(db_fetch)
+    assert_frame_equal(
+        merge,
+        pd.DataFrame(db_fetch),
+        check_like=True,
+    )
+
+
+def test_merge_ids_allow_dupdicate() -> None:
+    """Test PandasDataFrame.merge_ids() correctly merges ids."""
+    pandas_df = PandasDataFrame(pd.DataFrame({"a": ["A", "B", "C"]}))
+    db_fetch = [
+        {"a_id": 1, "a": "A", "b": 1},
+        {"a_id": 2, "a": "B", "b": 1},
+        {"a_id": 3, "a": "C", "b": 1},
+        {"a_id": 3, "a": "C", "b": 2},
+    ]
+    merge = pandas_df.merge_ids(db_fetch, allow_duplication=True)
+    assert_frame_equal(
+        merge,
+        pd.DataFrame(db_fetch),
+        check_like=True,
+    )
+
+
+def test_merge_ids_dupdicate_assertion() -> None:
+    """Test PandasDataFrame.merge_ids() correctly merges ids."""
+    pandas_df = PandasDataFrame(pd.DataFrame({"a": ["A", "B", "C"]}))
+    db_fetch = [
+        {"a_id": 1, "a": "A", "b": 1},
+        {"a_id": 2, "a": "B", "b": 1},
+        {"a_id": 3, "a": "C", "b": 1},
+        {"a_id": 3, "a": "C", "b": 2},
+    ]
+    with pytest.raises(AssertionError):
+        pandas_df.merge_ids(db_fetch)
