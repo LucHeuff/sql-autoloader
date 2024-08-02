e8a143249375568666f808d2b8ebd83f06014509
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-08-02 17:53:03 +0200

    update: testing of Schema with new class definitions

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,17 +1,38 @@
-from etl_components.schema import Column, Schema, Table
+import pytest
+
+from etl_components.schema import Reference, Schema, SchemaError, Table
+
+
+def test_table() -> None:
+    """Test if Table functions work as intended."""
+    name = "fiets"
+    sql = "CREATE fiets"
+    columns = ["id", "name"]
+    references = [Reference("fabriek_id", "fabriek", "id")]
+    referred_by = ["eignaar"]
+
+    table = Table(name, sql, columns, references, referred_by)
+
+    assert table.refers_to == ["fabriek"]
+    assert table.get_reference("fabriek") == references[0]
+    assert str(table) == sql
+    with pytest.raises(SchemaError):
+        table.get_reference("doos")
 
 
 def test_print_schema() -> None:
     """Test if schema_to_string formats the schema correctly."""
-    schema = Schema(
-        [
-            Table("fiets", [Column("ketting", "BOOL"), Column("zadel", "STR")]),
-            Table(
-                "trein",
-                [Column("pantograaf", "BOOL"), Column("locomotief", "STR")],
-            ),
-        ]
-    )
+    tables = [
+        Table("fiets", "CREATE fiets", ["", ""], [Reference("", "", "")], None),
+        Table(
+            "fabriek", "CREATE fabriek", ["", ""], [Reference("", "", "")], None
+        ),
+    ]
+    schema = Schema(tables)
 
-    schema_string = "TABLE fiets {\n  ketting: BOOL\n  zadel: STR\n}\n\nTABLE trein {\n  pantograaf: BOOL\n  locomotief: STR\n} "
-    assert str(schema) == schema_string.strip()
+    assert schema.table_names == ["fiets", "fabriek"]
+    for table in tables:
+        assert schema(table.name) == table
+    assert str(schema) == "CREATE fiets\n\nCREATE fabriek"
+    with pytest.raises(SchemaError):
+        schema("doos")
