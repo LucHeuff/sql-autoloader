48d85d9299763f3f0f880d229127ffb804581b71
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-08-06 21:20:23 +0200

    Got Connector functionality to work

--- etl_components/connector.py
+++ etl_components/connector.py
@@ -1,9 +1,13 @@
+import logging
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
-from typing import Iterator, Protocol, Self
+from copy import copy
+from typing import Any, Iterator, Protocol, Self
 
-from etl_components.dataframe import get_dataframe
+from etl_components.dataframe import DataFrame, get_dataframe
 from etl_components.schema import Schema
+
+logger = logging.getLogger(__name__)
 
 
 class Cursor(Protocol):
@@ -143,9 +147,10 @@ class DBConnector(ABC):
 
         Reference should be a dictionary of format
         {
-            "column": <name of reference column in current table>,
-            "table: <table that is referred to>,
-            "to": <column that is referred to>
+            "from_table": <name of the current table that does the referencing>
+            "from_column": <name of reference column in current table>,
+            "to_table: <table that is referred to>,
+            "to_column": <column that is referred to>
         }
 
         """
@@ -197,6 +202,14 @@ class DBConnector(ABC):
 
         common_columns = self.schema.parse_input(table, dataframe.columns)
         query = self.get_insert_query(table, common_columns)
+
+        logger.debug(
+            "Inserting %s into %s using query:\n\t%s",
+            common_columns,
+            table,
+            query,
+        )
+
         # Executing query
         with self.cursor() as cursor:
             cursor.executemany(
@@ -237,6 +250,12 @@ class DBConnector(ABC):
 
         common_columns = self.schema.parse_input(table, dataframe.columns)
         query = self.get_retrieve_query(table, common_columns)
+        logger.debug(
+            "Retrieving %s from %s using query:\n\t%s",
+            common_columns,
+            table,
+            query,
+        )
         # Executing query
         with self.cursor() as cursor:
             cursor.execute(query)
@@ -246,7 +265,7 @@ class DBConnector(ABC):
 
         if replace:
             # Use table schema to determine which non_id columns can be dropped.
-            schema_columns = self.schema(table).columns
+            schema_columns = self.schema.get_columns(table)
             non_id_columns = [col for col in schema_columns if "_id" not in col]
             dataframe.drop(non_id_columns)
         elif not replace and columns is not None:
@@ -254,9 +273,12 @@ class DBConnector(ABC):
             reverse_columns = {v: k for (k, v) in columns.items()}
             dataframe.rename(reverse_columns)
 
+        if isinstance(data, DataFrame):
+            return dataframe
+
         return dataframe.data
 
-    def insert_and_retrieve_ids(  # noqa ANN201
+    def insert_and_retrieve_ids(
         self,
         data,  # noqa: ANN001
         table: str,
@@ -264,7 +286,7 @@ class DBConnector(ABC):
         *,
         replace: bool = True,
         allow_duplication: bool = False,
-    ):
+    ) -> Any:  # noqa: ANN401
         """Insert data into database and retrieve ids to join them to data.
 
             data: DataFrame containing the data for which ids need to be retrieved and joined
@@ -294,22 +316,27 @@ class DBConnector(ABC):
     def compare(
         self,
         data,  # noqa: ANN001
-        query: str,
         columns: dict[str, str] | None = None,
+        query: str | None = None,
+        where: str | None = None,
         *,
-        exact: bool = False,
+        exact: bool = True,
     ) -> None:
         """Compare data in the database against data in a dataframe.
 
         Args:
         ----
             data: DataFrame containing data to be compared to
-            query: valid SQL query to retrieve data to compare to
             columns: (Optional) dictionary linking column names in data with column names in dataframe
                      Example {data_name: db_name, ...}
+            query: (Optional) valid SQL query to retrieve data to compare to.
+                   If left empty, will try to automatically generate a compare query.
+            where: (Optional) SQL WHERE clause to filter comparison data with.
+                   NOTE: please always prefix the tables for columns you are conditioning on.
             exact: (Optional) whether all the rows in data must match all
                    the rows retrieved from the database. If False, only checks
                    if rows from data appear in rows from query.
+            where:
 
         """
         dataframe = get_dataframe(data)
@@ -317,12 +344,16 @@ class DBConnector(ABC):
             dataframe.rename(columns)
         data_rows = dataframe.rows()
 
+        if query is None:
+            query = self.schema.get_compare_query(
+                dataframe.columns, where=where
+            )
+
+        logger.debug("Comparing using query:\n%s", query)
+
         with self.cursor() as cursor:
             cursor.execute(query)
             db_rows = cursor.fetchall()
-
-        # TODO These assertions check if the compare query ran well enough,
-        # this should not be necessary when automated.
 
         assert len(db_rows) > 0, "Compare query yielded no results."
         assert len(db_rows) >= len(
@@ -343,3 +374,56 @@ class DBConnector(ABC):
             assert all(
                 row in data_rows for row in db_rows
             ), "Not all rows in data appear in database."
+
+    # TODO do I need to be able to ignore columns?
+    def load(
+        self,
+        data,  # noqa: ANN001
+        columns: dict[str, str] | None = None,
+        *,
+        allow_duplication: bool = False,
+        where: str | None = None,
+        exact: bool = True,
+    ) -> None:
+        """Automatically load data into the database.
+
+        Args:
+        ----
+            data: DataFrame containing data to be inserted into the database.
+            columns: (Optional) translation of columns in data to column names in database.
+                     Dictionary of format {data_name: db_name}.
+                     If the same column name appears multiple times in the database,
+                     prefix the column name with the desired table, eg. <table>.<column_name>
+            allow_duplication: (Optional) whether joining on ids from the database can result in
+                                rows being duplicated.
+            where: (Optional) SQL WHERE clause to filter comparison data with.
+                   NOTE: please always prefix the tables for columns you are conditioning on.
+            exact: (Optional) whether all the rows in data must match all
+                   the rows retrieved from the database in comparison. If False, only checks
+                   if rows from data appear in rows from query.
+
+        """
+        dataframe = get_dataframe(data)
+        if columns is not None:
+            dataframe.rename(columns)
+
+        orig_dataframe = copy(dataframe)
+
+        insert_lists = self.schema.get_insert_and_retrieve_tables(
+            dataframe.columns
+        )
+
+        logger.debug("Inserting and retrieving tables...")
+        for table in insert_lists.insert_and_retrieve:
+            dataframe = self.insert_and_retrieve_ids(
+                dataframe, table, allow_duplication=allow_duplication
+            )
+
+        logger.debug("Inserting tables...")
+        for table in insert_lists.insert:
+            self.insert(dataframe, table)
+
+        logger.debug("Comparing...")
+        self.compare(orig_dataframe, where=where, exact=exact)
+
+        return dataframe.data
