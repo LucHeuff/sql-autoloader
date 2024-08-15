043220bfd40b8ef772837fd4779e6d2b2632b38b
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-08-15 08:40:38 +0200

    fix: adding access to SQLiteConnector

--- etl_components/__init__.py
+++ etl_components/__init__.py
@@ -1,3 +1,2 @@
-# from etl_components.connections import PostgresCursor, SQLiteCursor
-# from etl_components.interactions import insert, retrieve_ids, insert_and_retrieve_ids, compare
-
+"""Main etl_components imports."""
+from etl_components.sqlite_connector import SQLiteConnector
