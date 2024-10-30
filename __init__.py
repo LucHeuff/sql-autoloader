9a4a999fba92bc6e6be90077794f84b9ee285d55
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-10-30 15:38:00 +0100

    Removed __init__.py in the wrong place

--- __init__.py
+++
@@ -1,3 +0,0 @@
-"""Provides access to SQLiteConnector and PostgresConnector."""
-from .sql_autoloader.postgres_connector import PostgresConnector
-from .sql_autoloader.sqlite_connector import SQLiteConnector
