ec6150474f50608e541c7ff322c85d5a8271cdcb
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-10-30 15:15:19 +0100

    Attempt to improve package interface

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 """Provides access to SQLiteConnector and PostgresConnector."""
-from sql_autoloader.postgres_connector import PostgresConnector
-from sql_autoloader.sqlite_connector import SQLiteConnector
+from .sql_autoloader.postgres_connector import PostgresConnector
+from .sql_autoloader.sqlite_connector import SQLiteConnector
